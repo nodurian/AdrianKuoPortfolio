3072a69c77619af9c2e0f7b87f6633db1e6eba6b
nodurian <kuoadrian@gmail.com>
2019-08-13 02:50:58 -0700

    Fix mobile overflow; reduce margins on mobile

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,21 +6,21 @@ import { Component, OnInit, HostListener } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  cols: number;
+  colspan: number;
   outerRowHeight: string;
   innerRowHeight: string;
 
   constructor() { }
 
   ngOnInit() {
-    this.cols = (window.innerWidth <= 768) ? 1 : 2;
+    this.colspan = (window.innerWidth <= 768) ? 2 : 1;
     this.outerRowHeight = (window.innerWidth <= 768) ? "100px" : "2:0.8";
     this.innerRowHeight = (window.innerWidth <= 768) ? "35px" : "1:1";
   }
 
   @HostListener('window:resize', ['$event'])
   onResize(event) {
-    this.cols = (event.target.innerWidth <= 768) ? 1 : 2;
+    this.colspan = (event.target.innerWidth <= 768) ? 2 : 1;
     this.outerRowHeight = (window.innerWidth <= 768) ? "100px" : "2:0.8";
     this.innerRowHeight = (window.innerWidth <= 768) ? "35px" : "1:1";
   }
