a398a230136966232d1ef874784a7afaf7247ce5
nodurian <kuoadrian@gmail.com>
2019-08-12 17:36:27 -0700

    Finalize prototype; fix icon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// import { IntroComponent } from './intro/intro.component';
+// import { AboutComponent } from './about/about.component';
+// import { WorkComponent } from './work/work.component';
+// import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
+  // { path: '', redirectTo: '/home', pathMatch: 'full' },
+  // { path: 'home', component: IntroComponent },
+  // { path: 'about', component: AboutComponent },
+  // { path: 'work', component: WorkComponent },
+  // { path: 'contact', component: ContactComponent }
 ];
 
 @NgModule({
